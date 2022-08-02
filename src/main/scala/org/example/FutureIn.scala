0b714462932826d96bfbc8e41c1b15edf0e1fac9
YANG <yk__yang@outlook.com>
2022-08-02 23:24:13 +0800

    Future

--- src/main/scala/org/example/FutureIn.scala
+++ src/main/scala/org/example/FutureIn.scala
@@ -1,6 +1,7 @@
 package org.example
 
 
+import scala.annotation.tailrec
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
@@ -14,7 +15,7 @@ object FutureIn extends App {
   }
   def dowork(index: Int) = {
 //    随机一下
-    sleep((math.random * 1100).toLong)
+    sleep((math.random * 100).toLong)
     index
   }
 
@@ -31,6 +32,40 @@ object FutureIn extends App {
     }
   }
 //  等待所有任务执行完
-  sleep(1000)
+  sleep(8000)
 
+
+//  偏函数
+//  偏函数被包在花括号内没有match的一组case语句是一个偏函数
+//  偏函数是PartialFunction[A, B]的一个实例
+//  A代表输入参数类型,B代表返回结果类型
+  val function0: PartialFunction[Int, String] = {
+    case 1 => "一"
+    case 2 => "二"
+    case 3 => "三"
+    case _ => "其它"
+  }
+  println(function0(3))
+  println(function0(10))
+
+  val list = (0 to 10).toList
+  val list2 = list.map {
+    case x if x >= 1 && x <= 3 => "[1-3]"
+    case x if x >= 4 && x <= 8 => "[4-8]"
+    case x if x > 8 => "(8-*]"
+    case _ => "其它"
+  }
+  println(list2)
+
+//  递归
+  println(factorial(10))
+
+  def factorial(index:Int):Long = {
+    @tailrec
+    def fact(index: Int, acc: Int): Long={
+      if (index < 1) acc
+      else fact(index - 1, index * acc)
+    }
+    fact(index, 1)
+  }
 }
